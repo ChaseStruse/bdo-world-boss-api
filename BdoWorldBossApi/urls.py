5454f16dae20349809c8dcabef0fb4e7835a6645
Chase Struse <chasestruse@gmail.ocm>
2023-02-09 22:43:17 -0600

    Added worldboss endpoints

--- BdoWorldBossApi/urls.py
+++ BdoWorldBossApi/urls.py
@@ -4,5 +4,6 @@ from BdoWorldBossApi.Views import world_boss_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('worldboss/', world_boss_view.world_boss_list)
+    path('worldboss/', world_boss_view.world_boss_list),
+    path('worldboss/<int:_id>', world_boss_view.world_boss_detail)
 ]
