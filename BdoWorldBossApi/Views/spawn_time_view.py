5593a9c7bcd83e5059a69b2632a081a73897bb29
Chase Struse <chasestruse@gmail.ocm>
2023-02-09 23:03:26 -0600

    Got the bases done for spawn times

---
+++ BdoWorldBossApi/Views/spawn_time_view.py
@@ -0,0 +1,25 @@
+from rest_framework.decorators import api_view
+from BdoWorldBossApi.Services.spawn_time_service import SpawnTimeService
+
+_service = SpawnTimeService()
+
+
+@api_view(['GET', 'POST'])
+def spawn_time_list(request):
+    if request.method == 'GET':
+        return _service.get_all_spawn_times()
+
+    elif request.method == 'POST':
+        return _service.insert_spawn_time(request.data)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def spawn_time_detail(request, _id):
+    if request.method == 'GET':
+        return _service.get_spawn_time_by_id(_id)
+
+    elif request.method == 'PUT':
+        return _service.update_spawn_time(_id, request.data)
+
+    else:
+        return _service.delete_spawn_time(_id)
--- BdoWorldBossApi/urls.py
+++ BdoWorldBossApi/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from BdoWorldBossApi.Views import world_boss_view
+from BdoWorldBossApi.Views import world_boss_view, spawn_time_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('worldboss/', world_boss_view.world_boss_list),
-    path('worldboss/<int:_id>', world_boss_view.world_boss_detail)
+    path('worldboss/<int:_id>', world_boss_view.world_boss_detail),
+    path('spawntime/', spawn_time_view.spawn_time_list),
+    path('spawntime/<int:_id>', spawn_time_view.spawn_time_detail),
+
 ]
